49bc6f213c3f2c6764b5d7b1f3e73c2b4abd2c5f
pzoli <papp.zoltan@infokristaly.hu>
2025-10-27 21:36:55 +0100

    GlobalExceptionHandler

---
+++ src/main/java/hu/infokristaly/keycloakauthenticatoin/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package hu.infokristaly.keycloakauthenticatoin.exceptions;
+
+import hu.infokristaly.keycloakauthenticatoin.dtos.ErrorResponseDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponseDto> handleGlobalException(Exception exception,
+                                                                  WebRequest webRequest) {
+        ErrorResponseDto errorResponseDTO = new ErrorResponseDto(
+                webRequest.getDescription(false),
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                exception.getMessage(),
+                LocalDateTime.now()
+        );
+        return new ResponseEntity<>(errorResponseDTO, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
